41e127c39e8f5b3570b3b3c48da553cfc8f513a1
Sabin Neupane <61322712+sabin26@users.noreply.github.com>
2024-08-16 18:43:40 +0545

    Fix: Code analysis

--- server/lib/src/router/rest_router.dart
+++ server/lib/src/router/rest_router.dart
@@ -4,6 +4,7 @@ import 'package:ws_server/src/router/health_check.dart';
 import 'package:ws_server/src/router/long_polling.dart';
 import 'package:ws_server/src/router/not_found.dart';
 
-final Handler $restRouter = Router(notFoundHandler: $notFound)
-  ..get('/health', $healthCheck)
-  ..get('/long-polling', $longPolling);
+final Handler $restRouter = (Router(notFoundHandler: $notFound)
+      ..get('/health', $healthCheck)
+      ..get('/long-polling', $longPolling))
+    .call;
